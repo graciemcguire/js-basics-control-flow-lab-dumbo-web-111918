5b04a7f11d0be32682a0d2d784cf40c779efe954
Gracie McGuire <m.graciemcguire@gmail.com>
2019-01-05 23:57:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,5 @@ function scuberGreetingForFeet(feet) {
   if (feet > 2500){
     return "No can do."
   } 
-  else if (feet > 2000){
-    result += feet
-    return "I will gladly take your thirty bucks."
-  }
-  else (feet < 400) {
-    result += feet 
-    return "This one is on me!"
-  }
+
 }

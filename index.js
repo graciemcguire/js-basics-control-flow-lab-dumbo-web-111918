36edc0a159f7712adc23678dc555b104854fbd99
Gracie McGuire <m.graciemcguire@gmail.com>
2019-01-06 00:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,3 @@ function ternaryCheckCity(city) {
   return "Ok, sounds good."
   
 }
-
-const age = 17;
- 
-const isAdult = age >= 18 ? true : false;
- 
-const canWork = age >= 16 ? 1 === 1 : 1 !== 1;
- 
-const canEnlist = isAdult ? true : false;

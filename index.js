81c58a1ec415da32f9bb01eb302a3a2b087b7984
Gracie McGuire <m.graciemcguire@gmail.com>
2019-01-06 00:00:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ function scuberGreetingForFeet(feet) {
   }
 }
 
-function ternaryCheckCity() {
-  
+function ternaryCheckCity(city) {
+  const checkCity = city 
 }
+
+const age = 17;
+ 
+const isAdult = age >= 18 ? true : false;
+ 
+const canWork = age >= 16 ? 1 === 1 : 1 !== 1;
+ 
+const canEnlist = isAdult ? true : false;

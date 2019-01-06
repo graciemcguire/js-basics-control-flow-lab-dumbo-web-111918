453f6e301a4c5439a0d8cf7faa063ed019256ec2
Gracie McGuire <m.graciemcguire@gmail.com>
2019-01-06 00:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,25 +20,13 @@ function ternaryCheckCity(city) {
 function swithOnCharmFromTip(tip){
   switch(tip){
     case 'generous':
-      "Thank you so much!"
+      "Thank you so much.";
       break;
-    case 'not so generous':
-      "Thank you."
+    case 'not as generous':
+      "Thank you.";
       break;
     default:
-    'Bye.'
+    'Bye.';
   }
 }
 
- describe('switchOnCharmFromTip()', function () {
-    it('should return "Thank you so much." if the tip is generous', function () {
-      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
-    });
-
-    it('should return "Thank you." if the tip is not as generous', function () {
-      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
-    });
-
-    it('should return "Bye." if anything else', function () {
-      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
- 

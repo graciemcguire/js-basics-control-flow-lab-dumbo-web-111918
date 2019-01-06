f66320b0875df51c2da6f13d2bf0b0ccad0273ae
Gracie McGuire <m.graciemcguire@gmail.com>
2019-01-06 00:03:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function scuberGreetingForFeet(feet) {
 
 function ternaryCheckCity(city) {
   const checkCity = city === "New York" ? true : false;
-  return 
+  return "Ok, sounds good."
 }
 
 const age = 17;
